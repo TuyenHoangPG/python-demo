960a1f23c358f4790df1a6ede5a73c9db58fd17c
Tuyen Hoang <tuyen.hoangvan@powergatesoftware.com>
2022-07-25 10:01:32 +0700

    Update setup queue

--- src/api/v1/utils/notification_util.py
+++ src/api/v1/utils/notification_util.py
@@ -1,17 +1,19 @@
 """
     Emulate the function of sending notifications
 """
+from flask import current_app
 from api.v1.services.user_service import UserService
 import time
 
 
 def send_notification(user_id: str):
-    user_service = UserService()
-    users = user_service.get_all_user()
-    users = [user for user in users if user.id != user_id]
+    with current_app.app_context():
+        user_service = UserService()
+        users = user_service.get_all_user()
+        users = [user for user in users if user.id != user_id]
 
-    for user in users:
-        print(user.id)
-        time.sleep(1)
+        for user in users:
+            print(user.id)
+            time.sleep(1)
 
-    return len(users)
+        return len(users)
--- src/worker.py
+++ src/worker.py
@@ -3,6 +3,8 @@ import redis
 from rq import Worker, Queue, Connection
 from dotenv import load_dotenv
 
+from api import create_app
+
 load_dotenv()
 
 listen = ["default"]
@@ -10,6 +12,8 @@ redis_url = os.getenv("REDISTOGO_URL")
 redis_conn = redis.from_url(redis_url)
 
 if __name__ == "__main__":
-    with Connection(redis_conn):
-        worker = Worker(list(map(Queue, listen)))
-        worker.work()
+    app = create_app(os.getenv("FLASK_ENV", "development"))
+    with app.app_context():
+        with Connection(redis_conn):
+            worker = Worker(list(map(Queue, listen)))
+            worker.work()
